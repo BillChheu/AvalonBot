b94a8daa1da4d385a76333dab18d0e7ab6244d09
billchheu <tuh04091@temple.edu>
2020-06-10 16:45:57 -0500

    setup lobby and players

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
+
 const Discord = require("discord.js")
 const client = new Discord.Client();
+const auth = require("./token.json")
+
+class Player {
+    constructor(playerId, playerName) {
+        this.name = playerName;
+        this.id = playerId;
+    }
+}
 
 let gamestatus = 0;
 let lobby = [];
+let test = [];
 
 client.on("message", msg => {
 
-        if (msg.content === "Hello") {
-            msg.channel.send("World!")
-        }
+    
+
 
         if (msg.content === "!avalon") {
             if (gamestatus === 0) {
-                msg.channel.send("A game has started! \nUse !join to enter the lobby!\nUse !start to initiate the game")
+                msg.channel.send("A game has started! \nUse !join to enter the lobby!\nUse !start to begin the game")
                 gamestatus = 1
                 
+                addPlayer(msg.member.user.username, msg.member.id);
+               
+                showLobby();
+
             } else {
                 msg.reply("A game has already started!\nJoin the current game or wait for it to end!")
             }
-        } else if (msg.content === "!end") {
+        } else if (msg.content === "!join") {
+            addPlayer(msg.member.user.username, msg.member.id);
+            msg.reply("You have joined the lobby!");
+            //showLobby();
+        } else if (msg.content === "!showlobby") {
+            showLobby();
+        }
+
+        function showLobby() {
+            msg.channel.send("====================================\nLobby\n====================================\n")
             
+            for (let i = 0; i < lobby.length; i++) {
+                //msg.channel.send(test);
+                msg.channel.send(lobby[i].name +"\n");
+            }
         }
       
 })
 
-function addPlayer(player) {
+function addPlayer(playerName, playerId) {
+    let player = new Player(playerId, playerName);
     lobby.push(player)
+ 
 }
 
 
 
 
-
-
-client.login("NzIwMDAzNjQ3NzgxNDcwMzMw.Xt_uuQ.MkhAtbOZO1sZxUcU0_VxtpMgS_g")
+client.login(auth.token)
