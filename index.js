c91313a43990c3392d178ddd8069753b6be5894a
Bill <pbcomb1@gmail.com>
2020-06-18 14:09:12 -0400

    added quit command and fixed up showlobby

--- index.js
+++ index.js
@@ -7,55 +7,82 @@ class Player {
     constructor(playerId, playerName) {
         this.name = playerName;
         this.id = playerId;
+        this.role = 0;
     }
 }
 
 let gamestatus = 0;
 let lobby = [];
-let test = [];
+
 
 client.on("message", msg => {
 
-    
-
+    let messengerName = msg.member.user.username;
 
         if (msg.content === "!avalon") {
             if (gamestatus === 0) {
                 msg.channel.send("A game has started! \nUse !join to enter the lobby!\nUse !start to begin the game")
                 gamestatus = 1
                 
-                addPlayer(msg.member.user.username, msg.member.id);
+                addPlayer(messengerName, msg.member.id, showLobby);
                
-                showLobby();
 
             } else {
                 msg.reply("A game has already started!\nJoin the current game or wait for it to end!")
             }
+            
         } else if (msg.content === "!join") {
-            addPlayer(msg.member.user.username, msg.member.id);
             msg.reply("You have joined the lobby!");
-            //showLobby();
+            addPlayer(messengerName, msg.member.id, showLobby);   
+           
         } else if (msg.content === "!showlobby") {
             showLobby();
+
+        } else if (msg.content === "!quit") {
+            let temp = checkName(messengerName);
+            if (temp !== -1) {
+                removePlayer(temp);
+                msg.reply("you have been removed from the lobby!")
+            } else 
+                msg.reply("You are not in the lobby!");
         }
 
         function showLobby() {
             msg.channel.send("====================================\nLobby\n====================================\n")
+
+            let temp = "";
             
             for (let i = 0; i < lobby.length; i++) {
                 //msg.channel.send(test);
-                msg.channel.send(lobby[i].name +"\n");
+               temp += lobby[i].name + "\n";
             }
+            msg.channel.send(temp);
         }
       
 })
 
-function addPlayer(playerName, playerId) {
+function addPlayer(playerName, playerId, callback) {
     let player = new Player(playerId, playerName);
     lobby.push(player)
+    callback();
  
 }
 
+function removePlayer(player) {
+    let num = lobby.indexOf(player)
+    if (num > -1) {
+      lobby.splice(num, 1);  
+    }
+}
+
+function checkName(name) {
+    for (let i = 0; i < lobby.length; i++) {
+        if (lobby[i].name === name) {
+            return lobby[i];
+        }
+    }
+    return -1;
+}
 
 
 
