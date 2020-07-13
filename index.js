74a63e9d6427ca1e45c77f2ef5a373ce15cf356a
billchheu <tuh04091@temple.edu>
2020-07-13 16:45:22 -0500

    redid messaging evil players

--- index.js
+++ index.js
@@ -43,7 +43,29 @@ client.on("message", msg => {
     if (msg.author.bot)
         return;
 
-   // let messengerName = msg.member.user.username;
+    if (msg.content === "!end" && gamestatus > 0) {
+         gamestatus = 0;
+
+        lobby = [];
+        amtEvil = 0;
+        roundHistory = [];
+        playersNeededForQuest = [];
+        needTwoFails;
+
+        currentRound = 0;
+        successes = 0;
+        fails = 0;
+        playersOnQuest = [];
+        partyLeader;
+
+        votesOfPlayers = new Map();
+        failVotes = 0;
+        numFails = 1;
+
+        consecutiveVotingFailures = 0;
+
+        msg.channel.send("The game has ended!");
+    }
 
         if (msg.content === "!avalon") {
             if (gamestatus === 0) {
@@ -88,7 +110,7 @@ client.on("message", msg => {
 
            // TESTING STUFF WITH 2 PEOPLE
                 
-                amtEvil = 1;
+                amtEvil = 2;
                 playersNeededForQuest = [2,2,2,2,2];
                 needTwoFails = 1;
                 
@@ -132,7 +154,7 @@ client.on("message", msg => {
 
                 // assign evil roles and party leader (random)
                 msg.channel.send("The game has started!");
-                let evilMessage = "";
+                let evilPlayers = [];
                 let i = 0;
                 while (i < amtEvil) {
                     let evil = Math.floor(Math.random() * (numPlayers) )
@@ -141,23 +163,27 @@ client.on("message", msg => {
                             continue;
                     } else {
                         lobby[evil].roleOfPlayer = 1;
-                        evilMessage += lobby[evil].name + ", ";
+                        evilPlayers.push(lobby[evil]);
                         i++;
                     }
                 }
-                
-                evilMessage += "are evil!";
 
-                for (let i = 0; i < lobby.length; i++) {
-                    if (lobby[i].roleOfPlayer === 1) {
-                        lobby[i].user.send(evilMessage);
+                for (let i = 0; i < evilPlayers.length; i++) {
+                        evilPlayers[i].user.send("You are evil!");
+                    for (let j = 0; j < evilPlayers.length; j++) {
+                        if ( i === j) {
+                            continue;
+                        } else {
+                             evilPlayers[i].user.send(evilPlayers[j].name + " is also evil!");
+                        }
                     }
                 }
+                
+               
 
                 getNextPartyLeader();
                 msg.channel.send("<@" +partyLeader.id + "> is the party leader! Choose " + playersNeededForQuest[currentRound] + " players to go on this quest!");
 
-           return;
         }
                 // actual game
                 if ( (successes < 3 || fails < 3) && gamestatus === 2)  {
